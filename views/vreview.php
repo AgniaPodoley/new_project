793948539c8b4f8dc14b45b72fb91020d45b15da
lyubomyr83 <lyubomyr83@mail.ru>
2017-10-18 16:26:21 +0300

    Add styles for reviews

--- views/vreview.php
+++ views/vreview.php
@@ -20,10 +20,10 @@ if(!empty($review)){
         foreach ($value as $key=>$item){
             $res[$key]=$item;
         }
-        echo "<h3>".$res["name"]."</h3>";
-        echo "<p>Дата отзыва: ".date("d.m.Y \в H:i:s",$res["created"])."</p>";
-        echo "<div id=\"review\">".$res["review"]."</div>";
-        echo "<p class=\"review_autor\"><span class=\"review_bold\">Автор отзыва: </span><span class=\"review_italic\">".$res["autor"]."</span></p>";
+        echo "<h3 class=\"review_header\">".$res["name"]."</h3>";
+        echo "<p><i class=\"icon-table icon-large\"> </i>".date("d.m.Y",$res["created"])." <i class=\" icon-time icon-large\"> </i> ".date("H:i:s",$res["created"])."</p>";
+        echo "<blockquote><div id=\"review\">".$res["review"]."</div></blockquote>";
+        echo "<p class=\"review_autor\"><span class=\"review_bold\"></span><i class=\"icon-user icon-large\"> </i><span class=\"review_italic\">".$res["autor"]."</span></p>";
         echo "</p>";
     }
 }
